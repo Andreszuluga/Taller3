fef03e78233494a7b79210e1cc09b93434a78f8d
Andreszuluga <carlosandrescespedesz22@gmail.com>
2026-03-05 20:27:09 -0500

    Exercise 30

---
+++ taller3/MatrixOperations/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Ingrese orden de la matriz: ");
+        int n = int.Parse(Console.ReadLine()!);
+
+        int[,] matriz = new int[n, n];
+
+        int suma = 0;
+        int max = int.MinValue;
+        int min = int.MaxValue;
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                matriz[i, j] = (i + 1) - j;
+
+                suma += matriz[i, j];
+
+                if (matriz[i, j] > max)
+                    max = matriz[i, j];
+
+                if (matriz[i, j] < min)
+                    min = matriz[i, j];
+            }
+        }
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = 0; j < n; j++)
+            {
+                Console.Write(matriz[i, j] + "\t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine($"\nLa sumatoria es:  {suma}");
+        Console.WriteLine($"El valor máximo es: {max}");
+        Console.WriteLine($"El valor mínimo es: {min}");
+    }
+}
