ef1ff410aefb6a0e3547d8b3f22fcf106dc3487a
Andreszuluga <carlosandrescespedesz22@gmail.com>
2026-03-05 20:48:36 -0500

    Exercise 35

--- taller3/FactorDescomposition/Program.cs
+++ taller3/FactorDescomposition/Program.cs
@@ -1,2 +1,35 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Ingrese el número a descomponer: ");
+        int numero = int.Parse(Console.ReadLine()!);
+
+        int divisor = 2;
+
+        Console.Write(numero + " = ");
+
+        while (numero > 1)
+        {
+            if (numero % divisor == 0)
+            {
+                Console.Write(divisor);
+
+                numero = numero / divisor;
+
+                if (numero > 1)
+                {
+                    Console.Write(" x ");
+                }   
+            }
+            else
+            {
+                divisor++;
+            }
+        }
+
+        Console.ReadKey();
+    }
+}
